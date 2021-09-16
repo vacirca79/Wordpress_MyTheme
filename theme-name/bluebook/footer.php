4ea8d3d37a979d7653cd07cde4891bceadcc0acd
vacirca79 <vacircafr@gmail.com>
2021-09-16 14:22:10 +0200

    upload to my new git account

---
+++ theme-name/bluebook/footer.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bluebook' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'bluebook' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'theme: %1$s by %2$s.', 'bluebook' ), 'bluebook', '<a href="http://underscores.me/">franco vacirca</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ theme-name/bluebook/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bluebook
+ */
+
+get_header();
+
+?>
+<main id="primary" class="site-main">
+		<div id="main-content">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+		</div>
+		
+
+	</main><!-- #main -->
+
+<?php
+/* get_sidebar();*/
+get_footer();
